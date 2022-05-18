14381f873b06d5610f82653b46b1a26ed662b807
Diana Martinez <diananerdoficial@gmail.com>
2022-05-17 20:02:27 -0500

    JSON, attributos y metodos

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 console.log('Hola mundo :D')
+
+const persona = {
+  name: "Diana",
+  edad: 20,
+  saludar: function () {
+    console.log('Hola soy', this.name)
+  },
+  incrementarEdad() {
+    this.edad += 1
+  },
+  decirEdad: function () {
+    console.log('Mi edad es', this.edad)
+  }
+}
+
+const persona2 = {
+  name: "Candela",
+  edad: 20,
+}
+
+persona.saludar()
+persona.decirEdad()
